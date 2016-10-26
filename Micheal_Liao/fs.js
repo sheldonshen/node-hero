f6f6a5d02b920434890eb31a8185ff9064c641ef
sheldon shen <d87335903@users.noreply.github.com>
2016-10-26 14:16:17 +0800

    Node Stat object

--- Micheal_Liao/fs.js
+++ Micheal_Liao/fs.js
@@ -76,7 +76,32 @@ var fs = require("fs");
 var data = "Hello.node.js from sync writing";
 fs.writeFileSync("output.txt",data);
 
-//stat
-
+//stat(获取文件等相关信息)
+"use strict";
+var fs = require("fs");
+fs.stat("sample.txt",function(err,stat){
+	if(err){
+		console.log(err);
+	}else{
+		//是否是文件
+		console.log("isFile:"+stat.isFile());
+		//是否是目录
+		console.log("isDirectory:"+stat.isDirectory());
+		if(stat.isFile()){
+			//文件大小
+			console.log("size:" + stat.size);
+			//创建时间,Date对象
+			console.log("birth time:"+stat.birthtime);
+			//修改时间,Date对象
+			console.log("modified time:"+stat.mtime);
+		}
+	}
+});
+//stat也有一个对应的同步函数statSync
 
 //异步还是同步
+/*由于Node环境执行的JavaScript代码是服务器端代码，所以，绝大部分需要在服务器运行期反复执行业务逻辑的代码，
+必须使用异步代码，否则，同步代码在执行时期，服务器将停止响应，因为JavaScript只有一个执行线程*/
+
+/*服务器启动时如果需要读取配置文件，或者结束时需要写入到状态文件时，可以使用同步代码，
+因为这些代码只在启动和结束时执行一次，不影响服务器正常运行时的异步执行*/
